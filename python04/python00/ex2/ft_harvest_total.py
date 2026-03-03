66d2d1888f3a8020587329f981dfcdbaadd41c45
oussama khouya <98921677+oussama-khouya@users.noreply.github.com>
2026-03-03 17:41:41 +0000

    Delete python04/python00 directory

--- python04/python00/ex2/ft_harvest_total.py
+++
@@ -1,6 +0,0 @@
-def ft_harvest_total():
-    harv1 = int(input("Day 1 harvest: "))
-    harv2 = int(input("Day 2 harvest: "))
-    harv3 = int(input("Day 3 harvest: "))
-    total = harv1 + harv2 + harv3
-    print("Total harvest:", total)
