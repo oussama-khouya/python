abb186e0a65ac410aaad30d5685cbf6aa0f4d0f9
oussama khouya <98921677+oussama-khouya@users.noreply.github.com>
2026-03-03 17:41:50 +0000

    Delete python04/python01 directory

--- python04/python01/ex0/ft_garden_intro.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    print("=== Welcome to My Garden ===")
-    plant = "Rose"
-    Height = 25
-    age = 30
-    print(f"Plant: {plant}")
-    print(f"Height: {Height}cm")
-    print(f"Age: {age} days")
-    print("=== End of Program ===")
